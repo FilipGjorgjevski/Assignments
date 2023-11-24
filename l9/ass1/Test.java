1f22677a47ca0ab339e770c2b9b34d3bc0756516
FilipGjorgjevski <139113241+FilipGjorgjevski@users.noreply.github.com>
2023-11-24 18:12:54 +0100

    Create Test.java

---
+++ l9/ass1/Test.java
@@ -0,0 +1,23 @@
+
+public class Test {
+	String s;
+public Test()
+{
+	s="Programming Languages";
+}
+
+public void method1()
+{
+	System.out.println("The value of s:" + s);
+}
+public void method2(int n)
+{
+	System.out.println("The value of n:" + n);
+}
+private void method3()
+{
+	System.out.println("Private method is invoked");
+}
+
+}
+
